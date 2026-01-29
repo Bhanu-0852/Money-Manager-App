c709e7f98a93f8ad9c11868af1fc27c0616593c5
Bhanu prakash reddy <141407774+Bhanu-0852@users.noreply.github.com>
2026-01-29 20:59:03 +0530

    Update index.js

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -124,7 +124,7 @@ class MoneyManager extends Component {
       <div className="app-container">
         <div className="responsive-container">
           <div className="header-container">
-            <h1 className="heading">Hi, Bhanu</h1>
+            <h1 className="heading">Hello there!</h1>
             <p className="header-content">
               Welcome back to your
               <span className="money-manager-text"> Money Manager</span>
@@ -204,5 +204,6 @@ class MoneyManager extends Component {
     )
   }
 }
+
 
 export default MoneyManager
